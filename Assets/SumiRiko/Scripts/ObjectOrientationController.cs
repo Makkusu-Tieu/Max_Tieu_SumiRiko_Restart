37dfe6bf17c5ffdccd21145e748bb8bb5315d9d6
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2023-03-22 11:17:28 -0400

    Adjusted Object Orientation Script

--- Assets/SumiRiko/Scripts/ObjectOrientationController.cs
+++ Assets/SumiRiko/Scripts/ObjectOrientationController.cs
@@ -13,8 +13,6 @@ public class ObjectOrientationController : MonoBehaviour
 
     public GameObject socket;
 
-    private bool isInRange = false;
-
     private void Update()
     {
         Vector3 objectRotation = transform.rotation.eulerAngles;
@@ -23,35 +21,13 @@ public class ObjectOrientationController : MonoBehaviour
             && objectRotation.y >= minYAngle && objectRotation.y <= maxYAngle
             && objectRotation.z >= minZAngle && objectRotation.z <= maxZAngle)
         {
-            isInRange = true;
             socket.SetActive(true);
             // Remove any feedback mechanisms (e.g., stop highlighting the object)
         }
         else
         {
-            isInRange = false;
             socket.SetActive(false);
             // Add feedback mechanisms (e.g., highlight the object in red)
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == isInRange)
-        {
-            //socket.SetActive(true);
-            // Allow the object to be inserted into the socket
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == socket)
-        {
-            //isInRange = false;
-            //socket.SetActive(false);
-            // set isInRange to false when object exits the trigger
-            // Prevent the object from being inserted into the socket
         }
     }
 }
