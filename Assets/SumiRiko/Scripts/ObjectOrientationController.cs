50a9e5c8c9032b417df419fb2985b5896bd301f5
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2023-04-11 10:03:50 -0400

    Trying Rotation Script on Remote; Working on Y Rotation

--- Assets/SumiRiko/Scripts/ObjectOrientationController.cs
+++ Assets/SumiRiko/Scripts/ObjectOrientationController.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class ObjectOrientationController : MonoBehaviour
 {
-    //public float minXAngle = -90f;
-    //public float maxXAngle = 90f;
-    public float minYAngle = -90f;
+    public float minXAngle = 0f;
+    public float maxXAngle = 90f;
+
+    public float minYAngle = 0f;
     public float maxYAngle = 90f;
-    //public float minZAngle = -90f;
+
+    //public float minZAngle = 0f;
     //public float maxZAngle = 90f;
-    
+
     //For ranges that are only postive (example 0-90)
 
-    //public float minXAngle2 = -90f;
+    //public float minXAngle2 = 0f;
     //public float maxXAngle2 = 90f;
-    //public float minYAngle2 = -90f;
-    //public float maxYAngle2 = 90f;
-    //public float minZAngle2 = -90f;
+
+    public float minYAngle2 = 0f;
+    public float maxYAngle2 = 90f;
+
+    //public float minZAngle2 = 0f;
     //public float maxZAngle2 = 90f;
 
     //Enable only if your range goes from negative and postive (example 270-90)
@@ -29,12 +33,16 @@ public class ObjectOrientationController : MonoBehaviour
     {
         //Debug.Log("X Rotation is" + transform.eulerAngles.x);
         Debug.Log("Y Rotation is" + transform.eulerAngles.y);
+        //Debug.Log("Z Rotation is" + transform.eulerAngles.z);
         //Enable Each One at a Time to Figure Out Ranges
 
         Vector3 objectRotation = transform.rotation.eulerAngles;
 
-        if (/*(objectRotation.x >= minXAngle && objectRotation.x <= maxXAngle) || (objectRotation.x >= minXAngle2 && objectRotation.x <= maxXAngle2) &&*/
-            (objectRotation.y >= minYAngle && objectRotation.y <= maxYAngle)/* || (objectRotation.y >= minYAngle2 && objectRotation.y <= maxYAngle2)*/)
+        if ((objectRotation.x >= minXAngle && objectRotation.x <= maxXAngle)
+            &&
+            (objectRotation.y >= minYAngle && objectRotation.y <= maxYAngle)
+            || 
+            (objectRotation.y >= minYAngle2 && objectRotation.y <= maxYAngle2))
             //&& objectRotation.z >= minZAngle && objectRotation.z <= maxZAngle)
         {
             socket.SetActive(true);
