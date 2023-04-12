590b9e1885cb6dfb7b6d02ce07351bcb698067b7
Makkusu-Tieu <23tieuk@live.elida.k12.oh.us>
2023-04-12 09:59:17 -0400

    I THINK I GOT THE SCRIPT TO WORK YAAAAAAAAAAAY

--- Assets/SumiRiko/Scripts/ObjectOrientationController.cs
+++ Assets/SumiRiko/Scripts/ObjectOrientationController.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class ObjectOrientationController : MonoBehaviour
 {
+    //ATTACH THIS SCRIPT TO THE OBJECT, NOT THE SOCKET
+    //INSERT SOCKET INTO PUBLIC GAME OBJECT IN THE INSPECTOR
+
     public float minXAngle = 0f;
     public float maxXAngle = 90f;
 
     public float minYAngle = 0f;
     public float maxYAngle = 90f;
 
-    //public float minZAngle = 0f;
-    //public float maxZAngle = 90f;
+    public float minZAngle = 0f;
+    public float maxZAngle = 90f;
 
     //For ranges that are only postive (example 0-90)
 
-    //public float minXAngle2 = 0f;
-    //public float maxXAngle2 = 90f;
+    public float minXAngle2 = 0f;
+    public float maxXAngle2 = 90f;
 
     public float minYAngle2 = 0f;
     public float maxYAngle2 = 90f;
 
-    //public float minZAngle2 = 0f;
-    //public float maxZAngle2 = 90f;
+    public float minZAngle2 = 0f;
+    public float maxZAngle2 = 90f;
 
     //Enable only if your range goes from negative and postive (example 270-90)
     //For the example above, enable second set of variables and set one to (270-360) and another to (0-90)
@@ -32,28 +35,29 @@ public class ObjectOrientationController : MonoBehaviour
     private void Update()
     {
         //Debug.Log("X Rotation is" + transform.eulerAngles.x);
-        Debug.Log("Y Rotation is" + transform.eulerAngles.y);
+        //Debug.Log("Y Rotation is" + transform.eulerAngles.y);
         //Debug.Log("Z Rotation is" + transform.eulerAngles.z);
-        //Enable Each One at a Time to Figure Out Ranges
+        
+        //Enable Each One at a Time and Look at the Console to Figure Out Ranges
 
         Vector3 objectRotation = transform.rotation.eulerAngles;
 
-        if ((objectRotation.x >= minXAngle && objectRotation.x <= maxXAngle)
+        if (((objectRotation.x >= minXAngle && objectRotation.x <= maxXAngle) || (objectRotation.x >= minXAngle2 && objectRotation.x <= maxXAngle2))
             &&
-            (objectRotation.y >= minYAngle && objectRotation.y <= maxYAngle)
-            || 
-            (objectRotation.y >= minYAngle2 && objectRotation.y <= maxYAngle2))
+            ((objectRotation.y >= minYAngle && objectRotation.y <= maxYAngle) || (objectRotation.y >= minYAngle2 && objectRotation.y <= maxYAngle2))
+            &&
+            ((objectRotation.z >= minZAngle && objectRotation.z <= maxZAngle) || (objectRotation.z >= minZAngle2 && objectRotation.z <= maxZAngle2)))
             //&& objectRotation.z >= minZAngle && objectRotation.z <= maxZAngle)
         {
             socket.SetActive(true);
             Debug.Log("Object is in correct orientation");
-            // Remove any feedback mechanisms (e.g., stop highlighting the object)
+            // Things that Happen When It is In Range
         }
         else
         {
             socket.SetActive(false);
             Debug.Log("Object is not in correct orientation");
-            // Add feedback mechanisms (e.g., highlight the object in red)
+            // Things that Happen When It is Not In Range
         }
     }
 }
